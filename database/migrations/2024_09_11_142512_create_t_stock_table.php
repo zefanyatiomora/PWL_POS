0e8a7f6e8016bbd6be50f87cf8c1d68894c14da0
zefanya <zefanyatiomora@gmail.com>
2024-09-11 21:35:52 +0700

    JOBSHEET 3_PRAKTIKUM 2.2

---
+++ database/migrations/2024_09_11_142512_create_t_stock_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_stock', function (Blueprint $table) {
+            $table->id('stock_id');
+            $table->unsignedBigInteger('supplier_id')->index();
+            $table->unsignedBigInteger('barang_id')->index();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->datetime('stok_tanggal');
+            $table->integer('stok_jumlah');
+            $table->timestamps();
+
+            $table->foreign('supplier_id')->references('supplier_id')->on('m_supplier');
+            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
+            $table->foreign('user_id')->references('user_id')->on('m_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_stock');
+    }
+};
