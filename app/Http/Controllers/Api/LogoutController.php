1c5c07677fb5d8c8e04d8810b5b25f4386651326
zefanya <zefanyatiomora@gmail.com>
2024-10-28 10:28:30 +0700

    JOBSHEET 10_PRAKTIKUM 3

---
+++ app/Http/Controllers/Api/LogoutController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+class LogoutController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        // Remove token
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if ($removeToken) {
+            // Return response JSON
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Berhasil!'
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,7 @@ use App\Http\Controllers\Api\LoginController;
 */
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/logout', \App\Http\Controllers\Api\LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('user', function (Request $request) {
     return $request->user();
 });
