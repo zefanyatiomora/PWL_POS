668670f70e1a23eecd57d5521e6003000ef1fb32
zefanya <zefanyatiomora@gmail.com>
2024-10-28 09:56:21 +0700

    JOBSHEET 2_PRAKTIKUM 2

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use App\Http\Controllers\Api\RegisterController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('user', function (Request $request) {
+    return $request->user();
+});
